4a2444d184f871ecef992680493a5a1f6f48955d
ranjeet16pd <91504447+ranjeet16pd@users.noreply.github.com>
2023-07-03 10:52:13 +0530

    type any message here

--- server/index.js
+++ server/index.js
@@ -1,18 +1,17 @@
-import express from 'express';
-import router from './routes/routes.js';
-import cors from 'cors';
-import DBConnection from './database/db.js';
-const app=express();
-const Port =8000;
+import express from "express";
+import router from "./routes/routes.js";
+import cors from "cors";
+import DBConnection from "./database/db.js";
+const app = express();
+const Port = 8000;
 
 app.use(cors());
-app.use('/',router)
+app.use("/", router);
 
 DBConnection();
-app.get('/',async(req,res)=>{
-    res.send("this is the server ");
-})
-app.listen(Port,()=>{
-    console.log('server is running on port 8000');
-})
-
+app.get("/", async (req, res) => {
+  res.send("this is the server ");
+});
+app.listen(Port, () => {
+  console.log("server is running on port 8000");
+});
--- server/routes/image-controller.js
+++ server/routes/image-controller.js
@@ -9,10 +9,11 @@ export const uploadImage = async (request, response) => {
 
   try {
     const file = await File.create(fileObj);
+    response.status(200);
+    //   .json({ path: `https://sharenow-du5l.onrender.com/file/${file._id}` });
     response
       .status(200)
-    //   .json({ path: `https://sharenow-du5l.onrender.com/file/${file._id}` });
-      response.status(200).json({path:`http://localhost:8000/file/${file._id}` })
+      .json({ path: `https://shrnow.onrender.com/file/${file._id}` });
     //   console.log(file);
   } catch (error) {
     console.log("error on image-controler", error.message);
